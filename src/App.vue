cbc6374764b0ca8a38b53e594b567746e34b26a7
harry <koler778@gmail.com>
2022-01-16 13:24:05 +0800

    feat: update vant version

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,54 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="calendar-demo">
+    <van-cell-group inset>
+      <van-cell title="选择时间区间" :value="value" is-link @click="show=true" /> 
+    </van-cell-group>
+    <van-calendar
+      v-model:show="show"
+      type="range"
+      :allow-same-day="false"
+      :formatter="dayFormatter"
+      @confirm="handleConfirm"
+    />
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import { ref } from "vue";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+const show = ref(false);
+const value = ref('')
+const handleConfirm = (days) => {
+  const [start, end] = days
+  value.value = `${start.getMonth()+1}月${start.getDate()}日 - ${start.getMonth()+1}月${end.getDate()}日`
+  show.value = false
 }
+const dayFormatter = (day) => {
+  if (!day.date) {
+    return day;
+  }
+
+  const date = day.date.getDate();
+
+  if (date > 14 && date < 23 && date % 2 !== 0) {
+    day.type = "disabled";
+  }
+
+  if (day.type === "start") {
+    day.bottomInfo = "开始";
+  } else if (day.type === "end") {
+    day.bottomInfo = "结束";
+  }
+
+  return day;
+};
+
 </script>
 
 <style>
+body {
+  background-color: aliceblue;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
